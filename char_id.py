71c998269c2eb84d75535058d8ce4e6ca5d41760
Sorabh-Kumar <77542001+Sorabh-Kumar@users.noreply.github.com>
2021-03-15 02:54:21 -0700

    Add files via upload

---
+++ char_id.py
@@ -0,0 +1,9 @@
+ch= input()
+if(ch>='A' and ch<='Z'):
+ print("word is uppercase alphabet")
+elif(ch>='a' and ch<='z'):
+ print("word is lowercase alphabet")
+elif(ch>='0' and ch<='9'):
+ print("word is digit")
+else:
+  print("word is special character")
