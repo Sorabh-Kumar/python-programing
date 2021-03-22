a5db237bb36234e9c0bbf3d9051798c59903ef2f
Sorabh-Kumar <77542001+Sorabh-Kumar@users.noreply.github.com>
2021-03-22 03:25:44 -0700

    Add files via upload

--- prime.py
+++ prime.py
@@ -1,10 +1,10 @@
-n = int(input())
-c=2
-while c<n:
-    
- if n%c==0:
-     print("no. is not prime")
-     break
-     c+=1
+n= int(input('enter the number'))
+i=2
+t=0
+while i<n:
+    if n%i==0:
+        print('number is not a prime number')
+        break
+    i=i+1
 else:
-    print("number is prime")
+    print('number is a prime number')
